50fecdd06d7375fe2a74bac1e5982f35726ce610
abking <abkingwl@qq.com>
2018-02-26 19:00:30 +0800

    commit all

---
+++ js/components/footer/PlayStatus.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="status-footer">
+    <span v-if="status === READY">准备开始...</span>
+    <span v-if="status === PLAYING">游戏中...</span>
+    <a v-if="status === PASS" v-on:click.prevent.stop="reset" href>再玩一次</a>
+    <span class="elapsed">{{ elapsedMs }} 秒</span>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex'
+
+import { STATUS } from '../../vuex/store/statusEnum'
+
+export default {
+  data() {
+    return STATUS
+  },
+
+  computed: {
+    ...mapGetters(['status', 'elapsedMs'])
+  },
+
+  methods: {
+    ...mapActions(['reset'])
+  }
+}
+</script>
+
+<style scoped>
+.status-footer {
+  position: relative;
+  margin-top: 10px;
+  width: 100%;
+  height: 20px;
+  line-height: 20px;
+  text-align: center;
+  font-size: 18px;
+  font-weight: bold;
+}
+a {
+  text-decoration: none;
+}
+.elapsed {
+  position: absolute;
+  right: 10px;
+  font-size: 15px;
+  font-weight: normal;
+}
+</style>
